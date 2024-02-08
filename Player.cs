3f554bf97a6ec9a612ff5fa8ef7405e46922a37e
Jirru <jerzyraszk2000@gmail.com>
2024-02-08 21:41:01 +0100

    feat: player and movement added #3

---
+++ Player.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+	public const float Speed = 300.0f;
+	public const float JumpVelocity = -400.0f;
+	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		if (!IsOnFloor())
+		{
+
+			velocity.Y += gravity * (float)delta;
+		}
+
+		if (Input.IsActionJustPressed("jump") && IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+		}
+
+		velocity.X = Input.GetAxis("left", "right") * Speed;
+		Velocity = velocity;
+
+		MoveAndSlide();
+	}
+}
