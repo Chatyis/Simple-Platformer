76719f01f127066bc9fb0e71083d306e9c90ea99
Chatyis <chatisek2420@gmail.com>
2024-02-14 21:13:17 +0100

    feat: double jump, empowered jump added #13 #14

--- prefabs/player/Player.cs
+++ prefabs/player/Player.cs
@@ -6,6 +6,14 @@ public partial class Player : CharacterBody2D
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	public Timer jumpEmpowerTimer;
+
+	private bool canDoubleJump;
+
+	public override void _Ready()
+	{
+		jumpEmpowerTimer = GetNode<Timer>("Timer");
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -13,13 +21,30 @@ public partial class Player : CharacterBody2D
 
 		if (!IsOnFloor())
 		{
-
 			velocity.Y += gravity * (float)delta;
 		}
-
-		if (Input.IsActionJustPressed("jump") && IsOnFloor())
+		else
 		{
-			velocity.Y = JumpVelocity;
+			canDoubleJump = true;
+		}
+
+		if (Input.IsActionJustPressed("jump") && (IsOnFloor() || canDoubleJump))
+		{
+			if (!IsOnFloor())
+			{
+				canDoubleJump = false;
+				velocity.Y = JumpVelocity;
+			}
+			else
+			{
+				velocity.Y = JumpVelocity;
+				jumpEmpowerTimer.Start();
+			}
+		}
+
+		if (jumpEmpowerTimer.TimeLeft > 0f && Input.IsActionPressed("jump"))
+		{
+			velocity.Y -= gravity * 0.75f * (float)delta;
 		}
 
 		velocity.X = Input.GetAxis("left", "right") * Speed;
@@ -32,4 +57,9 @@ public partial class Player : CharacterBody2D
 	{
 		GetTree().ReloadCurrentScene();
 	}
+
+	// private void checkIfJumped()
+	// {
+
+	// }
 }
