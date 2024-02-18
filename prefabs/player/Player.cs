d9e671aa89ef229e199ffdcb8bc7017359929377
Michał Czyż <32648007+Chatyis@users.noreply.github.com>
2024-02-18 17:02:31 +0100

    feat: double jump, empowered jump added #13 #14
    * feat: double jump, empowered jump added #13 #14
    
    * feat: review fixes
    
    * feat: removed code repetition

--- prefabs/player/Player.cs
+++ prefabs/player/Player.cs
@@ -5,6 +5,14 @@ public partial class Player : CharacterBody2D
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	public Timer jumpEmpowerTimer;
+
+	private bool canDoubleJump;
+
+	public override void _Ready()
+	{
+		jumpEmpowerTimer = GetNode<Timer>("Timer");
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -12,13 +20,29 @@ public partial class Player : CharacterBody2D
 
 		if (!IsOnFloor())
 		{
-
 			velocity.Y += gravity * (float)delta;
 		}
+		else
+		{
+			canDoubleJump = true;
+		}
 
-		if (Input.IsActionJustPressed("jump") && IsOnFloor())
+		if (Input.IsActionJustPressed("jump") && (IsOnFloor() || canDoubleJump))
 		{
 			velocity.Y = JumpVelocity;
+			if (!IsOnFloor())
+			{
+				canDoubleJump = false;
+			}
+			else
+			{
+				jumpEmpowerTimer.Start();
+			}
+		}
+
+		if (jumpEmpowerTimer.TimeLeft > 0f && Input.IsActionPressed("jump"))
+		{
+			velocity.Y -= gravity * 0.75f * (float)delta;
 		}
 
 		velocity.X = Input.GetAxis("left", "right") * Speed;
