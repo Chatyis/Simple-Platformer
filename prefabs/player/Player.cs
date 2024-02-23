7f9231035730bc40895340340d2fe1893de3327d
Michał Czyż <32648007+Chatyis@users.noreply.github.com>
2024-02-23 10:05:22 +0100

    feat: added knockback and second life #20 #22

--- prefabs/player/Player.cs
+++ prefabs/player/Player.cs
@@ -5,19 +5,24 @@ public partial class Player : CharacterBody2D
 {
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
+	public const float KnockBackPower = 400.0f;
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
-	public Timer jumpEmpowerTimer;
-
-	private bool canDoubleJump;
-
+	private bool _canDoubleJump = true;
+	private bool _hasAdditionalLife = true;
+	private Timer _immunityWindowTimer;
+	private Timer _jumpEmpowerTimer;
+	private Timer _knockbackTimer;
+	
 	public override void _Ready()
 	{
-		jumpEmpowerTimer = GetNode<Timer>("Timer");
+		_jumpEmpowerTimer = GetNode<Timer>("JumpEmpowerTimer");
+		_knockbackTimer = GetNode<Timer>("KnockbackTimer");
+		_immunityWindowTimer = GetNode<Timer>("ImmunityWindowTimer");
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
+		var velocity = Velocity;
 
 		if (!IsOnFloor())
 		{
@@ -25,42 +30,77 @@ public partial class Player : CharacterBody2D
 		}
 		else
 		{
-			canDoubleJump = true;
+			_canDoubleJump = true;
 		}
 
-		if (Input.IsActionJustPressed("jump") && (IsOnFloor() || canDoubleJump))
+		if (Input.IsActionJustPressed("jump") && (IsOnFloor() || _canDoubleJump))
 		{
 			velocity.Y = JumpVelocity;
 			if (!IsOnFloor())
 			{
-				canDoubleJump = false;
+				_canDoubleJump = false;
 			}
 			else
 			{
-				jumpEmpowerTimer.Start();
+				_jumpEmpowerTimer.Start();
 			}
 		}
 
-		if (jumpEmpowerTimer.TimeLeft > 0f && Input.IsActionPressed("jump"))
+		if (_jumpEmpowerTimer.TimeLeft > 0 && Input.IsActionPressed("jump"))
 		{
 			velocity.Y -= gravity * 0.75f * (float)delta;
 		}
 
-		velocity.X = Input.GetAxis("left", "right") * Speed;
+		if (_knockbackTimer.TimeLeft == 0)
+		{
+			velocity.X = Input.GetAxis("left", "right") * Speed;
+		}
+
 		Velocity = velocity;
 
 		MoveAndSlide();
 	}
 
-	public void GameOver()
+	public void TakeDamage()
 	{
-		GlobalVar.Coins = 0;
-		GetTree().ReloadCurrentScene();
+		if (_immunityWindowTimer.TimeLeft == 0)
+		{
+			if (!_hasAdditionalLife)
+			{
+				GameOver();
+			}
+			_immunityWindowTimer.Start();
+			_hasAdditionalLife = false;
+		}
 	}
 
 	public void Win()
 	{
 		GetTree().ReloadCurrentScene();
 		GD.Print("You won!");
+	}
+
+	public void AddSecondLife()
+	{
+		_hasAdditionalLife = true;
+	}
+
+	public void Knockback(Vector2 enemyPosition)
+	{
+		var directionFromEnemy = Vector2.Zero;
+		var xDistanceFromEnemy = enemyPosition.X - GlobalPosition.X;
+		var normalisedDistanceFromEnemy = xDistanceFromEnemy / Mathf.Abs(xDistanceFromEnemy);
+		
+		directionFromEnemy.X = -normalisedDistanceFromEnemy * KnockBackPower;
+		directionFromEnemy.Y = -KnockBackPower / 2;
+		
+		Velocity = directionFromEnemy;
+		_knockbackTimer.Start();
+	}
+
+	private void GameOver()
+	{
+		GlobalVar.Coins = 0;
+		GetTree().ReloadCurrentScene();
 	}
 }
