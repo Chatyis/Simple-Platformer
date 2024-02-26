203c7a91565ab5882cecef273de04952d85709f6
Chatyis <chatisek2420@gmail.com>
2024-02-26 10:42:55 +0100

    feat: paused game text #30

--- prefabs/game_manager/GameManager.cs
+++ prefabs/game_manager/GameManager.cs
@@ -3,11 +3,19 @@ using System;
 
 public partial class GameManager : Node2D
 {
+	public CanvasLayer ui;
+
+	public override void _Ready()
+	{
+		ui = GetTree().Root.GetNode<CanvasLayer>("Main/Ui");
+	}
+
 	public override void _Process(double delta)
 	{
 		if (Input.IsActionJustPressed("pause"))
 		{
 			GetTree().Paused = !GetTree().Paused;
+			ui.CallDeferred("TogglePausedGameText");
 		}
 	}
 }
