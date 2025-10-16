ba173fe994593a3876097970d8b8fa4f2c28a914
Michał Czyż <32648007+Chatyis@users.noreply.github.com>
2025-10-16 15:06:54 +0200

    feat: Finishing touches (#42)

--- prefabs/checkpoint/Checkpoint.cs
+++ prefabs/checkpoint/Checkpoint.cs
@@ -2,11 +2,21 @@ using Godot;
 
 public partial class Checkpoint : Area2D
 {
+	[Export]
+	private string NextLevelName;
+	[Export]
+	private  bool WinningCheckpoint;
+	
 	private void OnBodyEntered(Node2D body)
 	{
-		if (body.Name == "Player")
+		if (!WinningCheckpoint && body.Name == "Player")
+		{
+			GetTree().ChangeSceneToFile("res://levels/"+NextLevelName+".tscn");
+		}
+		else if (body.Name == "Player")
 		{
 			body.CallDeferred("Win");
+
 		}
 	}
 }
